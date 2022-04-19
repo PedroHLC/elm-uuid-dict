f9e5913a9707cd3a834dc37fe6ec9cd4e925bbe2
PedroHLC <root@pedrohlc.com>
2022-04-19 09:03:39 -0300

    Wrongly removed functions (mistake in review)

--- src/UUID/Dict.elm
+++ src/UUID/Dict.elm
@@ -1,14 +1,17 @@
 module UUID.Dict exposing
     ( Dict
-    , empty, insert, update, remove
-    , member, get, size
-    , keys, fromList
-    , foldl
-    , toggle
+    , empty, singleton, insert, update, remove
+    , isEmpty, member, get, size
+    , keys, values, toList, fromList
+    , map, foldl, foldr, filter, partition
+    , union, intersect, diff, merge
+    , getFirst, findl, findr, toggle
     )
 
-{-| Fork of [elm-core's Dict](/packages/elm/core/1.0.5/Dict), bypassing the "comparable"-key limitation for `UUID`s.
-Also includes four extras.
+{-| Fork of [`elm/core`'s `Dict`](/packages/elm/core/1.0.5/Dict), bypassing the
+"comparable"-key limitation for [`UUID`s](/packages/TSFoster/elm-uuid/4.2.0/UUID#UUID).
+
+Also includes some extras.
 
 Insert, remove, and query operations all take _O(log n)_ time.
 
@@ -20,30 +23,32 @@ Insert, remove, and query operations all take _O(log n)_ time.
 
 # Build
 
-@docs empty, insert, update, remove
+@docs empty, singleton, insert, update, remove
 
 
 # Query
 
-@docs member, get, size
+@docs isEmpty, member, get, size
 
 
 # Lists
 
-@docs keys, fromList
+@docs keys, values, toList, fromList
 
 
 # Transform
 
-@docs foldl
+@docs map, foldl, foldr, filter, partition
 
 
 # Combine
 
+@docs union, intersect, diff, merge
+
 
 # Extra
 
-@docs toggle
+@docs getFirst, findl, findr, toggle
 
 -}
 
@@ -55,16 +60,58 @@ type NColor
     | Black
 
 
+{-| A dictionary of [`UUID`s](/packages/TSFoster/elm-uuid/4.2.0/UUID#UUID) and values.
+
+See [`elm/core`'s `Dict.Dict`](/packages/elm/core/1.0.5/Dict#Dict) for the original version.
+
+    import UUID
+    import UUID.Dict
+
+    initUsers : Seeds -> List User -> ( Seeds, UUID.Dict User )
+    initUsers seed1 =
+        let
+            ( id1, seed2 ) =
+                UUID.step seed1
+
+            ( id2, seed3 ) =
+                UUID.step seed2
+
+            ( id3, seed4 ) =
+                UUID.step seed2
+        in
+        UUID.Dict.fromList
+            [ ( id1, User "Alice" 28 1.65 )
+            , ( id2, User "Bob" 19 1.82 )
+            , ( id3, User "Chuck" 33 1.75 )
+            ]
+            |> Tuple.pair seed4
+
+    type alias User =
+        { name : String
+        , age : Int
+        , height : Float
+        }
+
+-}
 type Dict v
     = RBNode_elm_builtin NColor UUID v (Dict v) (Dict v)
     | RBEmpty_elm_builtin
 
 
+{-| Create an empty dictionary.
+-}
 empty : Dict v
 empty =
     RBEmpty_elm_builtin
 
 
+{-| Get the value associated with a key. If the key is not found, return
+`Nothing`. This is useful when you are not sure if a key will be in the
+dictionary.
+
+See [`elm/core`'s `Dict.get`](/packages/elm/core/1.0.5/Dict#get) for the original version.
+
+-}
 get : UUID -> Dict v -> Maybe v
 get targetKey dict =
     case dict of
@@ -83,6 +130,8 @@ get targetKey dict =
                     get targetKey right
 
 
+{-| Determine if a key is in a dictionary.
+-}
 member : UUID -> Dict v -> Bool
 member key dict =
     case get key dict of
@@ -93,6 +142,8 @@ member key dict =
             False
 
 
+{-| Determine the number of key-value pairs in the dictionary.
+-}
 size : Dict v -> Int
 size dict =
     sizeHelp 0 dict
@@ -108,6 +159,22 @@ sizeHelp n dict =
             sizeHelp (sizeHelp (n + 1) right) left
 
 
+{-| Determine if a dictionary is empty.
+isEmpty empty == True
+-}
+isEmpty : Dict v -> Bool
+isEmpty dict =
+    case dict of
+        RBEmpty_elm_builtin ->
+            True
+
+        RBNode_elm_builtin _ _ _ _ _ ->
+            False
+
+
+{-| Insert a key-value pair into a dictionary. Replaces value when there is
+a collision.
+-}
 insert : UUID -> v -> Dict v -> Dict v
 insert key value dict =
     case insertHelp key value dict of
@@ -166,6 +233,9 @@ balance color key value left right =
                     RBNode_elm_builtin color key value left right
 
 
+{-| Remove a key-value pair from a dictionary. If the key is not found,
+no changes are made.
+-}
 remove : UUID -> Dict v -> Dict v
 remove key dict =
     case removeHelp key dict of
@@ -285,7 +355,7 @@ removeMin dict =
 moveRedLeft : Dict v -> Dict v
 moveRedLeft dict =
     case dict of
-        RBNode_elm_builtin _ k v (RBNode_elm_builtin _ lK lV lLeft lRight) (RBNode_elm_builtin _ rK rV (RBNode_elm_builtin Red rlK rlV rlL rlR) rRight) ->
+        RBNode_elm_builtin clr k v (RBNode_elm_builtin lClr lK lV lLeft lRight) (RBNode_elm_builtin rClr rK rV ((RBNode_elm_builtin Red rlK rlV rlL rlR) as rLeft) rRight) ->
             RBNode_elm_builtin
                 Red
                 rlK
@@ -293,13 +363,23 @@ moveRedLeft dict =
                 (RBNode_elm_builtin Black k v (RBNode_elm_builtin Red lK lV lLeft lRight) rlL)
                 (RBNode_elm_builtin Black rK rV rlR rRight)
 
-        RBNode_elm_builtin _ k v (RBNode_elm_builtin _ lK lV lLeft lRight) (RBNode_elm_builtin _ rK rV rLeft rRight) ->
-            RBNode_elm_builtin
-                Black
-                k
-                v
-                (RBNode_elm_builtin Red lK lV lLeft lRight)
-                (RBNode_elm_builtin Red rK rV rLeft rRight)
+        RBNode_elm_builtin clr k v (RBNode_elm_builtin lClr lK lV lLeft lRight) (RBNode_elm_builtin rClr rK rV rLeft rRight) ->
+            case clr of
+                Black ->
+                    RBNode_elm_builtin
+                        Black
+                        k
+                        v
+                        (RBNode_elm_builtin Red lK lV lLeft lRight)
+                        (RBNode_elm_builtin Red rK rV rLeft rRight)
+
+                Red ->
+                    RBNode_elm_builtin
+                        Black
+                        k
+                        v
+                        (RBNode_elm_builtin Red lK lV lLeft lRight)
+                        (RBNode_elm_builtin Red rK rV rLeft rRight)
 
         _ ->
             dict
@@ -308,7 +388,7 @@ moveRedLeft dict =
 moveRedRight : Dict v -> Dict v
 moveRedRight dict =
     case dict of
-        RBNode_elm_builtin _ k v (RBNode_elm_builtin _ lK lV (RBNode_elm_builtin Red llK llV llLeft llRight) lRight) (RBNode_elm_builtin _ rK rV rLeft rRight) ->
+        RBNode_elm_builtin clr k v (RBNode_elm_builtin lClr lK lV (RBNode_elm_builtin Red llK llV llLeft llRight) lRight) (RBNode_elm_builtin rClr rK rV rLeft rRight) ->
             RBNode_elm_builtin
                 Red
                 lK
@@ -316,13 +396,23 @@ moveRedRight dict =
                 (RBNode_elm_builtin Black llK llV llLeft llRight)
                 (RBNode_elm_builtin Black k v lRight (RBNode_elm_builtin Red rK rV rLeft rRight))
 
-        RBNode_elm_builtin _ k v (RBNode_elm_builtin _ lK lV lLeft lRight) (RBNode_elm_builtin _ rK rV rLeft rRight) ->
-            RBNode_elm_builtin
-                Black
-                k
-                v
-                (RBNode_elm_builtin Red lK lV lLeft lRight)
-                (RBNode_elm_builtin Red rK rV rLeft rRight)
+        RBNode_elm_builtin clr k v (RBNode_elm_builtin lClr lK lV lLeft lRight) (RBNode_elm_builtin rClr rK rV rLeft rRight) ->
+            case clr of
+                Black ->
+                    RBNode_elm_builtin
+                        Black
+                        k
+                        v
+                        (RBNode_elm_builtin Red lK lV lLeft lRight)
+                        (RBNode_elm_builtin Red rK rV rLeft rRight)
+
+                Red ->
+                    RBNode_elm_builtin
+                        Black
+                        k
+                        v
+                        (RBNode_elm_builtin Red lK lV lLeft lRight)
+                        (RBNode_elm_builtin Red rK rV rLeft rRight)
 
         _ ->
             dict
@@ -338,6 +428,88 @@ update targetKey alter dictionary =
             remove targetKey dictionary
 
 
+singleton : UUID -> v -> Dict v
+singleton key value =
+    RBNode_elm_builtin Black key value RBEmpty_elm_builtin RBEmpty_elm_builtin
+
+
+{-| Combine two dictionaries. If there is a collision, preference is given
+to the first dictionary.
+-}
+union : Dict v -> Dict v -> Dict v
+union t1 t2 =
+    foldl insert t2 t1
+
+
+{-| Keep a key-value pair when its key appears in the second dictionary.
+Preference is given to values in the first dictionary.
+-}
+intersect : Dict v -> Dict v -> Dict v
+intersect t1 t2 =
+    filter (\k _ -> member k t2) t1
+
+
+{-| Keep a key-value pair when its key does not appear in the second dictionary.
+-}
+diff : Dict a -> Dict b -> Dict a
+diff t1 t2 =
+    foldl (\k v t -> remove k t) t1 t2
+
+
+{-| Combines two dictionaries.
+
+See [`elm/core`'s `Dict.merge`](/packages/elm/core/1.0.5/Dict#merge) for the original version.
+
+-}
+merge :
+    (UUID -> a -> result -> result)
+    -> (UUID -> a -> b -> result -> result)
+    -> (UUID -> b -> result -> result)
+    -> Dict a
+    -> Dict b
+    -> result
+    -> result
+merge leftStep bothStep rightStep leftDict rightDict initialResult =
+    let
+        stepState rKey rValue ( list, result ) =
+            case list of
+                [] ->
+                    ( list, rightStep rKey rValue result )
+
+                ( lKey, lValue ) :: rest ->
+                    case UUID.compare lKey rKey of
+                        LT ->
+                            stepState rKey rValue ( rest, leftStep lKey lValue result )
+
+                        GT ->
+                            ( list, rightStep rKey rValue result )
+
+                        EQ ->
+                            ( rest, bothStep lKey lValue rValue result )
+
+        ( leftovers, intermediateResult ) =
+            foldl stepState ( toList leftDict, initialResult ) rightDict
+    in
+    List.foldl (\( k, v ) result -> leftStep k v result) intermediateResult leftovers
+
+
+{-| Apply a function to all values in a dictionary.
+-}
+map : (UUID -> a -> b) -> Dict a -> Dict b
+map func dict =
+    case dict of
+        RBEmpty_elm_builtin ->
+            RBEmpty_elm_builtin
+
+        RBNode_elm_builtin color key value left right ->
+            RBNode_elm_builtin color key (func key value) (map func left) (map func right)
+
+
+{-| Fold over the key-value pairs in a dictionary from lowest key to highest key.
+
+See [`elm/core`'s `Dict.foldl`](/packages/elm/core/1.0.5/Dict#foldl) for the original version.
+
+-}
 foldl : (UUID -> v -> b -> b) -> b -> Dict v -> b
 foldl func acc dict =
     case dict of
@@ -348,6 +520,11 @@ foldl func acc dict =
             foldl func (func key value (foldl func acc left)) right
 
 
+{-| Fold over the key-value pairs in a dictionary from highest key to lowest key.
+
+See [`elm/core`'s `Dict.foldr`](/packages/elm/core/1.0.5/Dict#foldr) for the original version.
+
+-}
 foldr : (UUID -> v -> b -> b) -> b -> Dict v -> b
 foldr func acc t =
     case t of
@@ -358,11 +535,68 @@ foldr func acc t =
             foldr func (func key value (foldr func acc right)) left
 
 
+{-| Keep only the key-value pairs that pass the given test.
+-}
+filter : (UUID -> v -> Bool) -> Dict v -> Dict v
+filter isGood dict =
+    foldl
+        (\k v d ->
+            if isGood k v then
+                insert k v d
+
+            else
+                d
+        )
+        empty
+        dict
+
+
+{-| Partition a dictionary according to some test. The first dictionary
+contains all key-value pairs which passed the test, and the second contains
+the pairs that did not.
+-}
+partition : (UUID -> v -> Bool) -> Dict v -> ( Dict v, Dict v )
+partition isGood dict =
+    let
+        add key value ( t1, t2 ) =
+            if isGood key value then
+                ( insert key value t1, t2 )
+
+            else
+                ( t1, insert key value t2 )
+    in
+    foldl add ( empty, empty ) dict
+
+
+{-| Get all of the keys in a dictionary, sorted from lowest to highest.
+
+See [`elm/core`'s `Dict.keys`](/packages/elm/core/1.0.5/Dict#keys) for the original version.
+
+-}
 keys : Dict v -> List UUID
 keys dict =
-    foldr (\key _ keyList -> key :: keyList) [] dict
+    foldr (\key value keyList -> key :: keyList) [] dict
 
 
+{-| Get all of the values in a dictionary, in the order of their keys.
+
+See [`elm/core`'s `Dict.values`](/packages/elm/core/1.0.5/Dict#values) for the original version.
+
+-}
+values : Dict v -> List v
+values dict =
+    foldr (\key value valueList -> value :: valueList) [] dict
+
+
+{-| Convert a dictionary into an association list of key-value pairs, sorted by keys.
+-}
+toList : Dict v -> List ( UUID, v )
+toList dict =
+    foldr (\key value list -> ( key, value ) :: list) [] dict
+
+
+{-| Convert an association list into a dictionary.
+-}
 fromList : List ( UUID, v ) -> Dict v
 fromList assocs =
     List.foldl (\( key, value ) dict -> insert key value dict) empty assocs
@@ -372,7 +606,7 @@ fromList assocs =
 -- EXTRA
 
 
-{-| Like insert, but removes if already exists, discarding the new value too.
+{-| Like insert, but removes if the key already exists in the dictionary, discarding the new value too.
 -}
 toggle : UUID -> v -> Dict v -> Dict v
 toggle key value dict =
@@ -386,3 +620,65 @@ toggle key value dict =
                     Just value
         )
         dict
+
+
+{-| Retrieve the first node that matches a predicate.
+-}
+findl : (UUID -> v -> Bool) -> Dict v -> Maybe ( UUID, v )
+findl predicate dict =
+    let
+        find_ dict_ =
+            case dict_ of
+                RBEmpty_elm_builtin ->
+                    Nothing
+
+                RBNode_elm_builtin _ key value left right ->
+                    if predicate key value then
+                        Just ( key, value )
+
+                    else
+                        case find_ left of
+                            Nothing ->
+                                find_ right
+
+                            found ->
+                                found
+    in
+    find_ dict
+
+
+{-| Retrieve the last node that matches a predicate.
+-}
+findr : (UUID -> v -> Bool) -> Dict v -> Maybe ( UUID, v )
+findr predicate dict =
+    let
+        find_ dict_ =
+            case dict_ of
+                RBEmpty_elm_builtin ->
+                    Nothing
+
+                RBNode_elm_builtin _ key value left right ->
+                    if predicate key value then
+                        Just ( key, value )
+
+                    else
+                        case find_ right of
+                            Nothing ->
+                                find_ left
+
+                            found ->
+                                found
+    in
+    find_ dict
+
+
+{-| Retrieve the node with the smallest key.
+-}
+getFirst : Dict v -> Maybe ( UUID, v )
+getFirst dict =
+    case getMin dict of
+        RBNode_elm_builtin _ minKey minValue _ _ ->
+            Just ( minKey, minValue )
+
+        RBEmpty_elm_builtin ->
+            Nothing
--- src/UUID/Set.elm
+++ src/UUID/Set.elm
@@ -1,14 +1,16 @@
 module UUID.Set exposing
     ( Set
-    , empty, insert, remove
-    , member, size
-    , toList
-    , foldl
-    , toggle
+    , empty, singleton, insert, remove
+    , isEmpty, member, size
+    , union, intersect, diff
+    , toList, fromList
+    , map, foldl, foldr, filter, partition
+    , getFirst, toggle
     )
 
-{-| Fork of [elm-core's Set](/packages/elm/core/1.0.5/Set), bypassing the "comparable"-key limitation for `UUID`s.
-Also includes two extras.
+{-| Fork of [`elm/core`'s `Set`](/packages/elm/core/1.0.5/Set), bypassing the
+"comparable"-key limitation for [`UUID`s](/packages/TSFoster/elm-uuid/4.2.0/UUID#UUID).
+Also includes some extras.
 
 Insert, remove, and query operations all take _O(log n)_ time.
 
@@ -20,82 +22,188 @@ Insert, remove, and query operations all take _O(log n)_ time.
 
 # Build
 
-@docs empty, insert, remove
+@docs empty, singleton, insert, remove
 
 
 # Query
 
-@docs member, size
+@docs isEmpty, member, size
 
 
 # Combine
 
+@docs union, intersect, diff
+
 
 # Lists
 
-@docs toList
+@docs toList, fromList
 
 
 # Transform
 
-@docs foldl
+@docs map, foldl, foldr, filter, partition
 
 
 # Extra
 
-@docs toggle
+@docs getFirst, toggle
 
 -}
 
+import Set as Elm
 import UUID exposing (UUID)
 import UUID.Dict as Dict
 
 
+{-| Represents a set of unique [`UUID`s](/packages/TSFoster/elm-uuid/4.2.0/UUID#UUID).
+-}
 type Set
     = Set_uuid (Dict.Dict ())
 
 
+{-| Create an empty set.
+-}
 empty : Set
 empty =
     Set_uuid Dict.empty
 
 
+{-| Create a set with one `UUID`.
+-}
+singleton : UUID -> Set
+singleton key =
+    Set_uuid (Dict.singleton key ())
+
+
+{-| Insert a `UUID` into a set.
+-}
 insert : UUID -> Set -> Set
 insert key (Set_uuid dict) =
     Set_uuid (Dict.insert key () dict)
 
 
+{-| Remove a `UUID` from a set. If the `UUID` is not found, no changes are made.
+-}
 remove : UUID -> Set -> Set
 remove key (Set_uuid dict) =
     Set_uuid (Dict.remove key dict)
 
 
+{-| Determine if a set is empty.
+-}
+isEmpty : Set -> Bool
+isEmpty (Set_uuid dict) =
+    Dict.isEmpty dict
+
+
+{-| Determine if an `UUID` is in a set.
+-}
 member : UUID -> Set -> Bool
 member key (Set_uuid dict) =
     Dict.member key dict
 
 
+{-| Determine the number of elements in a set.
+-}
 size : Set -> Int
 size (Set_uuid dict) =
     Dict.size dict
 
 
+{-| Get the union of two sets. Keep all values.
+-}
+union : Set -> Set -> Set
+union (Set_uuid dict1) (Set_uuid dict2) =
+    Set_uuid (Dict.union dict1 dict2)
+
+
+{-| Get the intersection of two sets. Keeps values that appear in both sets.
+-}
+intersect : Set -> Set -> Set
+intersect (Set_uuid dict1) (Set_uuid dict2) =
+    Set_uuid (Dict.intersect dict1 dict2)
+
+
+{-| Get the difference between the first set and the second. Keeps values
+that do not appear in the second set.
+-}
+diff : Set -> Set -> Set
+diff (Set_uuid dict1) (Set_uuid dict2) =
+    Set_uuid (Dict.diff dict1 dict2)
+
+
+{-| Convert a set into a list, sorted from lowest to highest.
+-}
 toList : Set -> List UUID
 toList (Set_uuid dict) =
     Dict.keys dict
 
 
+{-| Convert a list into a set, removing any duplicates.
+-}
+fromList : List UUID -> Set
+fromList list =
+    List.foldl insert empty list
+
+
+{-| Fold over the values in a set, in order from lowest to highest.
+-}
 foldl : (UUID -> b -> b) -> b -> Set -> b
 foldl func initialState (Set_uuid dict) =
     Dict.foldl (\key _ state -> func key state) initialState dict
+
+
+{-| Fold over the values in a set, in order from highest to lowest.
+-}
+foldr : (UUID -> b -> b) -> b -> Set -> b
+foldr func initialState (Set_uuid dict) =
+    Dict.foldr (\key _ state -> func key state) initialState dict
+
+
+{-| Map a function onto an [`elm/core`'s `Set`](/packages/elm/core/1.0.5/Set),
+creating a new set with no duplicates.
+-}
+map : (UUID -> comparable) -> Set -> Elm.Set comparable
+map func set =
+    Elm.fromList (foldl (\x xs -> func x :: xs) [] set)
+
+
+{-| Only keep elements that pass the given test.
+
+See [`elm/core`'s `Set.filter`](/packages/elm/core/1.0.5/Set#filter) for the original version.
+
+-}
+filter : (UUID -> Bool) -> Set -> Set
+filter isGood (Set_uuid dict) =
+    Set_uuid (Dict.filter (\key _ -> isGood key) dict)
+
+
+{-| Create two new sets. The first contains all the elements that passed the
+given test, and the second contains all the elements that did not.
+-}
+partition : (UUID -> Bool) -> Set -> ( Set, Set )
+partition isGood (Set_uuid dict) =
+    let
+        ( dict1, dict2 ) =
+            Dict.partition (\key _ -> isGood key) dict
+    in
+    ( Set_uuid dict1, Set_uuid dict2 )
 
 
 
 -- EXTRA
 
 
-{-| Like insert, but removes if already exists, discarding the new value too.
+{-| Like insert, but removes if the key already exists int the set, discarding the new value too.
 -}
 toggle : UUID -> Set -> Set
 toggle key (Set_uuid dict) =
     Set_uuid (Dict.toggle key () dict)
+
+
+{-| Retrieve the node with the smallest key.
+-}
+getFirst : Set -> Maybe UUID
+getFirst (Set_uuid dict) =
+    Maybe.map Tuple.first (Dict.getFirst dict)
